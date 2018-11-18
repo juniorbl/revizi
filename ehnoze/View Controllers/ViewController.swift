7cf807c6430381a600ec89b4d9ab7bbc7c36fc6d
Carlos Luz <carlosluzjunior@gmail.com>
2018-11-17 19:09:59 -0500

    start of the development, blank project

---
+++ ehnoze/View Controllers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ehnoze
+//
+//  Created by Carlos on 2018-11-17.
+//  Copyright © 2018 Carlos Luz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
